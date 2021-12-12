dcfe2a7fcc8a3cc4b4be47a56bc4a0cad17d8993
DenisKiarie <denis.kiarie@student.moringaschool.com>
2021-12-12 15:36:59 +0300

    Added a javascript file

---
+++ script.js
@@ -0,0 +1,18 @@
+function validate(){
+    var date = document.forms["fillForm"]["date"].value;
+    var gender = document.forms["fillForm"]["gender"].value;
+
+    if(date == "") {
+        alert("enter date");
+        return false;
+    }
+
+    if(gender == ""){
+        alert("select a gender");
+        return false;
+    }
+
+    
+
+}
+
